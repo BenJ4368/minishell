675e76f212261d39863ae602092163eca5288cde
Benjamin Gaertner <bgaertne@k2r1p6.42mulhouse.fr>
2023-10-23 15:02:52 +0200

    sanitize input

--- src/sanitize_input.c
+++ src/sanitize_input.c
@@ -6,7 +6,7 @@
 /*   By: bgaertne <bgaertne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 13:56:05 by ssalor            #+#    #+#             */
-/*   Updated: 2023/10/23 14:42:37 by bgaertne         ###   ########.fr       */
+/*   Updated: 2023/10/23 15:01:15 by bgaertne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	sanitize_input(t_data *data)
 {
 	char	**tab;
-	char	*new_input;s
+	char	*new_input;
 	int		i;
 	int		malloc_len;
 
@@ -24,8 +24,18 @@ void	sanitize_input(t_data *data)
 	i = -1;
 	while (tab[++i])
 		malloc_len += ft_strlen(tab[i]) + 1;
+	printf("%s\n", tab[0]);
 	new_input = ft_calloc(sizeof(char), malloc_len);
-	i = -1
+	i = -1;
 	while (tab[++i])
-		ft_strlcat(new_input, tab[i])
+	{
+		ft_strlcat(new_input, tab[i], malloc_len);
+		if (tab[i + 1])
+			ft_strlcat(new_input, " ", malloc_len);
+		printf("tab: %s\n", tab[i]);
+		printf("ni: %s\n", new_input);
+	}
+	printf("%s\n", new_input);
+	free(data->input);
+	data->input = new_input;
 }
